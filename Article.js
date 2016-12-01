78d679ee096b47329fa3d47995c4dd64d26a44bf
tlind15 <tlind15@gmail.com>
2016-11-30 22:08:59 -0800

    Create Article.js

---
+++ Article.js
@@ -0,0 +1,12 @@
+Article = function(author, link, article, agency) {
+    this.author = author;
+    this.link = link;
+    this.article = function (article) {
+        //summarize
+    };
+    this.agency = agency;
+};
+
+Article.prototype.display = function (text) {
+    //display
+};
