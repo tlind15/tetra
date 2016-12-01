3bc9ffebcb0b50b53e1841ae5333fb8dee868dec
tlind15 <tlind15@gmail.com>
2016-12-01 14:45:20 -0800

    Display multiple definitions including <d_link>

--- Search.js
+++ Search.js
@@ -26,24 +26,49 @@ Search.prototype.fetchArticle = function (criteria) {
 
 Search.prototype.fetchDefinition = function (criteria) {
     /*Access word, part of speech, definition, call theasaurus apiand get 5 synonyms. repeat for each word in criteria. if criteria > 5 cant do it */
-    var words = [];
-            $.ajax({
-                url : "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/despair?key=cdd30743-539d-4551-8374-7e7f133cffa7",
-                dataType : "xml",
-                async: false,
-                success : function(parsed_xml) {
-                    //document.write((new XMLSerializer()).serializeToString(parsed_xml));
-                    //alert(xml2json(parsed_xml, "\t"));
-                    var data = (JSON.parse(xml2json(parsed_xml, '\t')));
-                    document.write(JSON.stringify(data));
-                },
-                error:function() {
-                    alert("Error");
+    var words = ["despair"];
+    for (var k=0; k < words.length; k++) {
+        $.ajax({
+            url : "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + words[k] + "?key=cdd30743-539d-4551-8374-7e7f133cffa7",
+            dataType : "xml",
+            async: false,
+            success : function(parsed_xml) {
+                //document.write((new XMLSerializer()).serializeToString(parsed_xml));
+                var defs = parsed_xml.getElementsByTagName("dt");
+                for (var j=0; j < defs.length; j++) {
+                    if (j > 0 && parsed_xml.getElementsByTagName("ew")[j].childNodes[0].nodeValue !=
+                        parsed_xml.getElementsByTagName("ew")[j-1].childNodes[0].nodeValue) {
+                        break;
+                    }
+                    if (j == 0)
+                        alert(parsed_xml.getElementsByTagName("ew")[j].childNodes[0].nodeValue); //word
+
+                    alert(parsed_xml.getElementsByTagName("fl")[j].childNodes[0].nodeValue); //part of speech
+
+                    var data = parsed_xml.getElementsByTagName("dt")[j].childNodes;
+                    var text = "";
+                    var tag = "";
+                    for (var i = 0; i < data.length; i++) {
+                        tag = parsed_xml.getElementsByTagName("dt")[j].childNodes[i].tagName;
+                        if (tag === undefined) {
+                            text += data[i].nodeValue;
+                        }
+
+                        else if (typeof(text) === 'string') {
+                            if (tag == "d_link")
+                                text += parsed_xml.getElementsByTagName("d_link")[j].childNodes[0].nodeValue;
+                        }
+                    }
+                    alert(text);
                 }
-            });
+            },
+            error:function() {
+                alert("Error");
+            }
+        });
+    }
 
 };
-
 
 
 Search.prototype.fetchCollege = function (criteria) {
