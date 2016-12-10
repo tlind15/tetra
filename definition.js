a1fba957d09f59a8398e71ef7d4ee60fa21c80de
tlind15 <tlind15@gmail.com>
2016-12-10 13:26:50 -0800

    add definition.js

---
+++ definition.js
@@ -0,0 +1,10 @@
+Definition = function (word, partOfSpeech, definition, synonym) {
+    this.word = word;
+    this.partOfSpeech = partOfSpeech;
+    this.definition = definition;
+    this.synonym = synonym;
+};
+
+Definition.prototype.display = function() {
+    //decide how to display
+};
