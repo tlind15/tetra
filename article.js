37e80fd64e4950f1fb93e0a0ba6c9f531428b637
tlind15 <tlind15@gmail.com>
2016-12-12 21:35:59 -0800

    upload article.js

---
+++ article.js
@@ -0,0 +1,9 @@
+Article = function(title, article, link) {
+    this.title = title;
+    this.article = article;
+    this.link = link;
+};
+
+Article.prototype.display = function (text) {
+    //display
+};
